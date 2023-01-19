3cf7a6ee73da13c7b2601103f80b2017ffe4df9f
Deivid Gonçalves <deividg2022@outlook.com>
2023-01-19 00:00:59 -0300

    feat swagger

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
 const db = require('./database');
 const Horarios = require('./models/Horarios');
 const Estabelecimentos = require('./models/Estabelecimentos');
+const SwaggerDocs = require('../swagger.json');
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(routes);
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(SwaggerDocs))
 
 
      db.authenticate().then(() => {
